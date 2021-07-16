9d4461bd8bf7d380a59055453a4afd3688cd38cb
agugomv <agugomv@gmail.com>
2021-07-16 02:13:33 -0300

    mail

--- src/main/java/com/m8/event/manager/controller/EventoController.java
+++ src/main/java/com/m8/event/manager/controller/EventoController.java
@@ -3,51 +3,39 @@ package com.m8.event.manager.controller;
 import com.m8.event.manager.entity.Evento;
 import com.m8.event.manager.repository.EventoRepository;
 import com.m8.event.manager.service.EventoService;
-import jdk.jfr.Event;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
 @RequestMapping("/eventos")
 public class EventoController {
 
-@Autowired
-private EventoService es;
+    @Autowired
+    private EventoService es;
 
-@Autowired
-private EventoRepository rp;
+    @Autowired
+    private EventoRepository rp;
 
     @GetMapping()
-    public ModelAndView eventos(){
+    public ModelAndView eventos() {
         ModelAndView mav = new ModelAndView("eventos");
-        mav.addObject("title","Lista de eventos");
+        mav.addObject("title", "Lista de eventos");
         return mav;
     }
 
-
     @GetMapping("/ver/{id}")
-    public ModelAndView verEvento(@PathVariable("id") Integer id){
+    public ModelAndView verEvento(@PathVariable("id") Integer id) {
 
-            ModelAndView mav = new ModelAndView("evento-detalle");
-            Evento e = rp.findById(id).get();
-        mav.addObject("title",e.getNombre() + " - EventManager");
-        mav.addObject("evento",e);
+        ModelAndView mav = new ModelAndView("evento-detalle");
+        Evento e = rp.findById(id).get();
+        mav.addObject("title", e.getNombre() + " - EventManager");
+        mav.addObject("evento", e);
         return mav;
 
-
-        }
-
-
-
-
-
-
+    }
 
 }
